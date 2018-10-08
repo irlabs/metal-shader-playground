3e7486596c260e537e3dfd713b371917b773b2d3
Dirk van Oosterbosch <dirk@innovactory.com>
2018-10-08 08:43:43 +0200

    New MacOS app as testbed for Metal

---
+++ MetalBox/MetalBox/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  MetalBox
+//
+//  Created by Dirk van Oosterbosch on 08/10/2018.
+//  Copyright © 2018 IRLabs. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
