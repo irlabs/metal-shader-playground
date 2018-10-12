762b605fc349efb3a68935f9ef917db48f8e64d4
Dirk van Oosterbosch <dirk@innovactory.com>
2018-10-13 01:26:02 +0200

    Created cross-platform Metal Shader example

---
+++ MetalBox/MetalBox iOS/GameViewController.swift
@@ -0,0 +1,64 @@
+//
+//  GameViewController.swift
+//  MetalBox iOS
+//
+//  Created by Dirk van Oosterbosch on 13/10/2018.
+//  Copyright © 2018 IRLabs. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+
+class GameViewController: UIViewController {
+
+    var gameView: SCNView {
+        return self.view as! SCNView
+    }
+    
+    var gameController: GameController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.gameController = GameController(sceneRenderer: gameView)
+        
+        // Allow the user to manipulate the camera
+        self.gameView.allowsCameraControl = true
+        
+        // Show statistics such as fps and timing information
+        self.gameView.showsStatistics = true
+        
+        // Configure the view
+        self.gameView.backgroundColor = UIColor.black
+        
+        // Add a tap gesture recognizer
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        var gestureRecognizers = gameView.gestureRecognizers ?? []
+        gestureRecognizers.insert(tapGesture, at: 0)
+        self.gameView.gestureRecognizers = gestureRecognizers
+    }
+    
+    @objc
+    func handleTap(_ gestureRecognizer: UIGestureRecognizer) {
+        // Highlight the tapped nodes
+        let p = gestureRecognizer.location(in: gameView)
+        gameController.highlightNodes(atPoint: p)
+    }
+    
+    override var shouldAutorotate: Bool {
+        return true
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+
+}
